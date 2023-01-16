b6dc5f06d5cb59c4207c11baa3993850caa16c09
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-16 14:35:44 +0900

    prime_number_vr.3

--- day03.py
+++ day03.py
@@ -1,27 +1,12 @@
-#prime number
-
-number = int(input("input number : "))
-counts = 0
-k = 1
-
-while k <= number:
-    if number % k == 0:
-        counts = counts + 1
-    k=k+1
-if counts == 2:
-    print(f'{number} is prime number!')
-else:
-    print(f'{number} is NOT prime number!')
-
-#prime number _ for
+#prime number _ for2
 
 number = int(input("input number : "))
 counts = 0
 
-for k in range(1,number+1):
+for k in range(2,number):
     if number % k == 0:
         counts = counts + 1
-if counts == 2:
-    print(f'{number} is prime number!')
-else:
+if counts: #0이면 False 0이 아니면 True
     print(f'{number} is NOT prime number!')
+else:
+    print(f'{number} is prime number!')
