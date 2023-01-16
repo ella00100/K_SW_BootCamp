5aef34c5d7bf66f508424a40d3a828a3e1fd3d7a
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-16 12:46:00 +0900

    sting_func

--- day03.py
+++ day03.py
@@ -1,17 +1,43 @@
-print("I'm a girl")
+univ = "Inha University"
+print(len(univ)) #길이 구하기 len
+print(univ.split()) #문자열 나누기 split
 
-army = '우리는 국가와 국민에 충성을 다하는 대한민국 육군이다.' \
-       '하나 우리는 자유민주주의를 수호하여 조국 콩일의 역국이 된다.'
-print(army)
+#문자열 추출
+print(univ[5:-6])
+print(univ[5:])
+print(univ[5:15])
+print(univ[-10:])
+print(univ[::2])
+print(univ[0:15:2])
 
-army2 = '''우리는 국가와 국민에 충성을 다하는 대한민국 육군이다.
-       하나 우리는 자유민주주의를 수호하여 조국 콩일의 역국이 된다.'''
-print(army2)
+#문자열 합치기  .join
+pokemons_list = ['피카츄', '꼬부기', '파이리','이상해씨']
+pokemeons_string = ', '.join(pokemons_list)
+print(pokemeons_string)
 
-army3 = '우리는 국가와 국민에 충성을 다하는 대한민국 육군이다.\n하나 우리는 자유민주주의를 수호하여 조국 콩일의 역국이 된다.'
-print(army3)
+#문자열 교체  .replace
+inha = 'a duck goes into a bar'
+print(inha.replace('a', 'a famous'))
+print(inha.replace('a', 'a famous',1))
 
-start = 'Na' * 4 + '\n'
-middle = 'hey' *2 + '\n'
-end = 'bye'
-print(start+middle+end)
+#문자열 제거 .strip
+subjects = '!   python, data structure, database   !'
+print(subjects)
+print(subjects.strip('!'))
+Oh = 'What the ..!?!'
+print(Oh.strip('!?!'))
+
+#문자열 검색
+subjects = '$ python, data struct, database    $$$'
+print(subjects.find('data'),subjects.index('data'))
+print(subjects.rfind('data'),subjects.rindex('data'))
+print(subjects.count('data'))
+print(subjects.find('inha')) #없으면 -1 출력
+# print(subjects.index('inha')) #없으면 오류 발생
+
+#정렬
+setup = '안녕하세요'
+print(setup.center(30))
+print(setup.rjust(30))
+print(setup.ljust(30))
+
