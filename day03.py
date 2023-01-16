8381ecbdcbdb38d1fc85df587d2854ef43f26ba9
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-16 13:44:03 +0900

    구구단

--- day03.py
+++ day03.py
@@ -1,5 +1,25 @@
-#prob5.3
-print("My kitty cat likes %s, "
-      "\nMy kitty cat like %s,"
-      "\nMy kitty cat fell on his %s And now thinks he's a %s"
-      % ('roast beef', 'ham', 'head', 'clam'))
+#구구단
+
+while True:
+    dan = int(input('Dan : '))
+    if 1 < dan <10:
+        i = 1
+        while i < 10:
+            print('{0} * {1} = {2}'.format(dan,i,dan*i))
+            i = i+1
+        break
+    else:
+        print('2에서 9사이의 값을 입력 하세요')
+
+#반대로!
+
+while True:
+    dan = int(input('Dan :'))
+    if dan < 2 or dan > 9 :
+        print('2에서 9사이의 값을 입력 하세요')
+    else:
+        i = 1
+        while i <10:
+            print('{0} * {1} = {2}'.format(dan, i, dan * i))
+            i = i + 1
+        break
