a1a0c4285ee11f3c490573d591c384fcd0befaf4
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-16 14:15:05 +0900

    구구단_for

--- day03.py
+++ day03.py
@@ -1,25 +1,14 @@
-#구구단
+#구구단_for
 
 while True:
     dan = int(input('Dan : '))
+
+    if dan == 0:
+        break
     if 1 < dan <10:
         i = 1
-        while i < 10:
+        for i in range(1,10):
             print('{0} * {1} = {2}'.format(dan,i,dan*i))
-            i = i+1
-        break
     else:
         print('2에서 9사이의 값을 입력 하세요')
 
-#반대로!
-
-while True:
-    dan = int(input('Dan :'))
-    if dan < 2 or dan > 9 :
-        print('2에서 9사이의 값을 입력 하세요')
-    else:
-        i = 1
-        while i <10:
-            print('{0} * {1} = {2}'.format(dan, i, dan * i))
-            i = i + 1
-        break
