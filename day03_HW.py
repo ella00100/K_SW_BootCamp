cdf2e4b34dc47cbbd1d0f6dbf636f92fd40fe8c7
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-16 15:53:01 +0900

    prob5.2

--- day03_HW.py
+++ day03_HW.py
@@ -1,18 +1,18 @@
-#prob 5.1
+#prob 5.2
 
-song = """When an eel grabs your arm, 
-And it cause great harm,
-That's -a moray"""
+questions = [
+    "We don't serve string around here. Are you a String?",
+    "What is said on Father's Day in the forest?",
+    "What makes the sound 'Sis! Boom! Bah!'?"
+]
 
-#방법 1
-print(song.replace(' m',' M'))
+answers = [
+    "An exploding sheep.",
+    "No, I'm frayed knot.",
+    "'Pop!' goes the weasel."
+]
 
-#방법 2
-idx = song.rfind('m')
-print(song[0:idx]+song[idx].upper()+song[idx+1:])
-
-#방법3
-song_list = song.split()
-song_list[13] = song_list[13].title()
-song_string = ' '.join(song_list)
-print(song_string)
+i=0
+while i<3:
+    print('Q: ',questions[i], '\nA: ',answers[i])
+    i = i+1
