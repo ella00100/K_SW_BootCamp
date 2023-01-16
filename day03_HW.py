bf1ddb6aecec6aeef578b3cc4c850b4ee51711e2
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-16 16:40:31 +0900

    prob5.6

--- day03_HW.py
+++ day03_HW.py
@@ -1,19 +1,5 @@
-#prob 5.4 & 5.5
-salutation = 'Mr.'
-name = 'jake'
-product = 'chocolate'
-verbed = 'delayed'
-room  = 'house'
-animals = 'animals'
-amount = '10'
-percent = '30'
-spokesman = 'Ella'
-job_title = 'Marketing Manager'
+#prob5.6
+subject = ['duck','gourd','spitz']
 
-letter = print('  Dear {} {},\n\n'.format(salutation,name),
-               ' Thank you for your letter. We are sorry that our {} {} in your {}.'.format(product,verbed,room),
-               'Please note that it should never be used in a {}, especially near any {}.\n\n'.format(room,animals),
-               ' Send us your receipt and {} for shipping and handling'.format(amount),
-               'We will send you another {} that, in out tests, is {}% less likely to have {}\n\n'.format(product,percent,verbed),
-               ' Thank you for your support.\n  Sincerely,\n  {}\n  {}'.format(spokesman,job_title)
-)
+for i in range(0,3):
+    print('%sy Mc%sface' % (subject[i].capitalize(), subject[i].capitalize()))
