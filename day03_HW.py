f8fb9f598131f6c61342584421d11e011e955868
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-16 15:53:57 +0900

    prob5.3

--- day03_HW.py
+++ day03_HW.py
@@ -1,18 +1,6 @@
-#prob 5.2
+#prob 5.3
 
-questions = [
-    "We don't serve string around here. Are you a String?",
-    "What is said on Father's Day in the forest?",
-    "What makes the sound 'Sis! Boom! Bah!'?"
-]
-
-answers = [
-    "An exploding sheep.",
-    "No, I'm frayed knot.",
-    "'Pop!' goes the weasel."
-]
-
-i=0
-while i<3:
-    print('Q: ',questions[i], '\nA: ',answers[i])
-    i = i+1
+print("My kitty cat likes %s, "
+      "\nMy kitty cat like %s,"
+      "\nMy kitty cat fell on his %s And now thinks he's a %s"
+      % ('roast beef', 'ham', 'head', 'clam'))
