964e348aa884f7d08bf5698282a3b7bf4f824b5a
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-17 15:20:19 +0900

    dictionay_ex_random_format

--- day04.py
+++ day04.py
@@ -1,12 +1,8 @@
 import random
 
-alcohol_foods = {
-    'beer' : 'chicken',
-    'soju' : 'tomato',
-    'wine' : 'cheese',
-    'makguli' : 'fish & Chips'
-}
+alcohol_foods = dict(beer = 'chicken',soju = 'tomato', wine ='cheese',makguli = 'fish & Chips')
 alcohol_list = list(alcohol_foods)
+
 food_list = [food for food in alcohol_foods.values()]
 while True:
     alcohol = int(input(f"술을 선택 하세요 1){alcohol_list[0]} 2){alcohol_list[1]} 3){alcohol_list[2]} 4){alcohol_list[3]} 5)랜덤! 6)종료 : "))
