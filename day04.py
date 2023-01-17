acb4228746c1da94f408692e4f9cd760a65c2cab
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-17 13:39:37 +0900

    list comprehension

--- day04.py
+++ day04.py
@@ -1,6 +1,13 @@
-rows =range(1,4)
-cols = range(1,3)
+#list comprehension
 
-cells = [(row,col) for row in rows for col in cols]
-for cell in cells:
-    print(cell)
+odd_lists = []
+for i in range(1,11):
+    if i % 2 == 1:
+        odd_lists.append(i)
+print(odd_lists)
+
+odd_lists = [i for i in range(1,11) if i % 2 == 1]
+print(odd_lists, type(odd_lists))
+
+odd_tuples = (i for i in range(1,11) if i % 2 == 1)
+print(odd_tuples, type(odd_tuples))
