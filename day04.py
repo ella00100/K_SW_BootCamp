aed74b3103546f492070258f1d314e73254c79fb
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-17 14:25:07 +0900

    dictionay

--- day04.py
+++ day04.py
@@ -1,13 +1,22 @@
-#list comprehension
+#dictionary
+students = {'name':'Ella', 'age': 23, 'eyes': [0.9, 1.1]}
 
-odd_lists = []
-for i in range(1,11):
-    if i % 2 == 1:
-        odd_lists.append(i)
-print(odd_lists)
+for k in students:  #default로 key를 출력
+    print(k)
+print("\n")
 
-odd_lists = [i for i in range(1,11) if i % 2 == 1]
-print(odd_lists, type(odd_lists))
+for k in students.keys(): #key 출력 .keys
+    print(k)
+print("\n")
 
-odd_tuples = (i for i in range(1,11) if i % 2 == 1)
-print(odd_tuples, type(odd_tuples))
+for k in students.values(): #value 출력  .values
+    print(k)
+print("\n")
+
+for k, v in students.items(): #key와 value 출력 .items
+    print(f'{k} : {v}')
+print("\n")
+
+#dictionary[key][위치]
+print(f'이름은 {students["name"]}, 나이는 {students["age"]}세', end = "\n")
+print(f'시력은 좌 : {students["eyes"][0]}, 우 : {students["eyes"][1]}')
