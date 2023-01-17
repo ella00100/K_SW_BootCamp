fcc8d29deede8206a03675a7d82fd010a0091c24
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-17 16:32:20 +0900

    prob 7.10

--- day04_HW.py
+++ day04_HW.py
@@ -1,9 +1,3 @@
-#prob 7.8
-surprise = ["Groucho", "Chico", "Harpo"]
-
-#prob 7.9
-surprise[-1] = surprise[-1].lower()
-harpo = list(surprise[-1])
-rv_harpo = ''.join(harpo[::-1])
-surprise[-1] = rv_harpo.title()
-print(surprise)
+#prob 7.10
+even_list = [number for number in range(10) if number % 2 == 0]
+print(even_list)
