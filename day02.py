4627dd77a174c4458b20a9ebea4ebc999c7f9665
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-17 13:09:57 +0900

    type

--- day02.py
+++ day02.py
@@ -1,12 +1,14 @@
-harry_poter={'헤르미온느':'그리핀도르',
-             '말포이': '슬리데린',
-             '말포이':'그리핀도르'}
-print(harry_poter)
-print(harry_poter['말포이']) #dictionary는 key:value = 1:1
-print(type(harry_poter))
+PI = 3.14
+print(f'원주율의 값은 {PI}이고 타입의 {type(PI)}입니다')
 
-harry_poter={'헤르미온느':'그리핀도르',
-             '말포이': '슬리데린',
-             '론':'그리핀도르'}
-print(harry_poter)
-print(type(harry_poter))
+math_values = (3.24, 2.71) #튜플 생성
+print(f'원주율의 값은 {math_values}이고 타입은 {type(math_values)}입니다)')
+math_values[0] = 9.99 #튜플은 값을 변경할 수 없음
+print(f'원주율의 값은 {math_values}이고 타입은 {type(math_values)}입니다)')
+#에러 발생
+
+math_values = [3.24, 2.71] #리스트 생성
+print(f'원주율의 값은 {math_values}이고 타입은 {type(math_values)}입니다)')
+math_values[0] = 9.99 #리스트는 값을 변경할 수 있음
+print(f'원주율의 값은 {math_values}이고 타입은 {type(math_values)}입니다)')
+#9.99 2.71 출력
