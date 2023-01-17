d572e3066afd893655b2b89f7d32104a44424c6e
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-17 15:10:11 +0900

    dictionay_ex_random

--- day04.py
+++ day04.py
@@ -1,22 +1,22 @@
-#dictionary
-students = {'name':'Ella', 'age': 23, 'eyes': [0.9, 1.1]}
+import random
 
-for k in students:  #default로 key를 출력
-    print(k)
-print("\n")
-
-for k in students.keys(): #key 출력 .keys
-    print(k)
-print("\n")
-
-for k in students.values(): #value 출력  .values
-    print(k)
-print("\n")
-
-for k, v in students.items(): #key와 value 출력 .items
-    print(f'{k} : {v}')
-print("\n")
-
-#dictionary[key][위치]
-print(f'이름은 {students["name"]}, 나이는 {students["age"]}세', end = "\n")
-print(f'시력은 좌 : {students["eyes"][0]}, 우 : {students["eyes"][1]}')
+alcohol_foods = {
+    'beer' : 'chicken',
+    'soju' : 'tomato',
+    'wine' : 'cheese',
+    'makguli' : 'fish & Chips'
+}
+alcohol_list = list(alcohol_foods)
+food_list = [food for food in alcohol_foods.values()]
+while True:
+    alcohol = int(input(f"술을 선택 하세요 1){alcohol_list[0]} 2){alcohol_list[1]} 3){alcohol_list[2]} 4){alcohol_list[3]} 5)랜덤! 6)종료 : "))
+    if alcohol == 6:
+        print('다음에 또 오세요~')
+        break
+    elif alcohol in (1,2,3,4) :
+        print(f'{alcohol_list[alcohol-1]}에 어울리는 안주는 {alcohol_foods[alcohol_list[alcohol-1]]}입니다')
+        pass
+    elif alcohol == 5:
+        print(f'{random.choice(alcohol_list)}와 {random.choice(food_list)}는 어떠세요?')
+    else:
+        print('메뉴에서 골라주세요')
