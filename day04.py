ed47d9605f4fcf0faeb6475e7b79241fc6a92339
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-17 13:27:03 +0900

    for_in

--- day04.py
+++ day04.py
@@ -1,30 +1,6 @@
-#list
-import copy
-a = [1, 2, [5, -9]]
-b = copy.deepcopy(a)
-a[2][1] = 7  #mutable, deepcopy
-print(a,b)
+rows =range(1,4)
+cols = range(1,3)
 
-
-a = [1, 2, [5, -9]]
-b = a.copy()
-c = list(a)
-d = a[:]
-a[2][1] = 7  #mutable, b/c/d affects
-print(a,b,c,d)
-
-
-a = [1, 2, 3]
-b = a.copy()
-c = list(a)
-d = a[:]
-a[2] = 'sw'
-print(a,b,c,d)
-
-
-#cp
-primes = [2, 19, 3.0, 5, 7, 11]
-primes_cp = primes
-print(primes_cp)
-primes[-1] = 'lunch time'
-print(primes_cp)
+cells = [(row,col) for row in rows for col in cols]
+for cell in cells:
+    print(cell)
