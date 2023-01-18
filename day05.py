58c956cf21727d5af1ab08563b3c221aa68a40c1
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-18 14:10:51 +0900

    calculate_fee_vr.3

--- day05.py
+++ day05.py
@@ -1,7 +1,7 @@
 import random
-def calculate_fee2(args):   #매개변수를 리스트로 받음
+def calculate_fee3(args):   #매개변수를 리스트로 받음
     """
-    놀이공원 요금 계산 프로그램 vr.2
+    놀이공원 요금 계산 프로그램 vr.3
     :param args: ages in list
     :return: [total fee, adults, children]  #반환 값 리스트로
     """
@@ -15,10 +15,10 @@ def calculate_fee2(args):   #매개변수를 리스트로 받음
         else:
             children += 1
             total_fee = total_fee + 3000
-    return [len(args), adults, children, total_fee]
+    return dict(total_c=len(args), adult_c = adults, child_c =children, total_f = total_fee)
 
-no_of_visitor = int(input("몇 분이세요? "))
+no_of_visitor = int(input("몇 분이세요?"))
 ages = [random.randint(1,60) for age in range(no_of_visitor)]
-results = calculate_fee2(ages)
+results = dict(calculate_fee3(ages))
 
-print(f'총 {results[0]}분이서 방문하셨고 어른 {results[1]}명, 아이 {results[2]}명, 총 요금은 {results[3]}원 입니다.')
+print(f'총 {results["total_c"]}분이서 방문하셨고 어른 {results["adult_c"]}명, 아이 {results["child_c"]}명, 총 요금은 {results["total_f"]}원 입니다.')
