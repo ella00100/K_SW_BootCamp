36371b2ba1f44e8840ee0214193603d9b429f822
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-18 11:46:52 +0900

    calculate_fee

--- day05.py
+++ day05.py
@@ -1,31 +1,16 @@
-#function : prime number
-
-#함수 정의
-def isprime(n):
+def calculate_fee(*args):
     """
-    매개변수로 받은 정수(n)가 소수인가를 판정하는 함수
-    :param n : integer number
-    :return  : true of false
+    놀이공원 요금 계산 프로그램
+    :param args: ages(가변 type)
+    :return: total fee
     """
-    if n <= 1:
-        return False
-    for k in range(2,n):
-        if n % k == 0:
-            return False
-    else:
-        return True
+    total=0
+    for age in args:
+        if 19 <= age:
+            total = total + 10000
+        else:
+            total = total + 3000
+    return total
 
-#isprime 작동 확인
-print(isprime(7))  #True
-print(isprime(10)) #False
-help(isprime) #함수 내부의 주석처리한 부분 출력
-
-#프로그램 시작
-start = int(input("input start number : "))
-end = int(input("input end number : "))
-
-if end < start :
-    start, end = end, start
-for i in range(start, end+1):
-    if isprime(i):
-        print(i, end = ' ')
+print(calculate_fee(20, 20, 25))
+print(calculate_fee(15, 20))
