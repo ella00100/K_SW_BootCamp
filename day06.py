9d3cdbf3b713275eaf3c25d245d4aed8a787cd5e
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-19 13:43:58 +0900

    day06_try-except

--- day06.py
+++ day06.py
@@ -1,25 +1,26 @@
-def factorial_iter(n):
+def div_calc(a,b):
     """
-    반복문을 이용한 팩터리얼 함수
-    :param n: n!
-    :return:  integer 팩토리얼 계산 결과 값
+    나누기 연산 함수
+    :param a: 분자
+    :param b: 분모
+    :return: 계산 결과
     """
-    result = 1
-    for k in range(1,n+1):
-        result = result * k
-    return  result
+    return a/b
+print(div_calc(15,3))
 
-def fatorial_recu(n):
-    """
-    재귀 함수를 이용한 팩토리얼 함수
-    :param n: n!
-    :return:  자기 자신을 호출 또는 1
-    """
-    if n == 1:
-        return 1
-    else:
-        return fatorial_recu(n-1)*n
-
-
-print(factorial_iter(5))
-print(fatorial_recu(5))
+try:
+    expr = input("분자 분모를 입력해 주세요(띄어쓰기로 구분): ").split()
+    print(int(expr[0]) /int(expr[1]))
+    # print(int(expr[4])) index error
+except ValueError as err:
+    print(f"숫자만 입력 가능합니다.({err})")
+except ZeroDivisionError as err:
+    print(f"분모에 0이 올 수 없습니다. ({err})")
+except IndexError as err:
+    print(f"입력 값의 범위에 문제가 있습니다. ({err})")
+except Exception as other:
+    print(f"알 수 없는 오류가 발생했습니다. ({other})")
+else: #예외가 발생하지 않았을 때 동작
+    print('프로그램이 정상적으로 작동했습니다.', end = '\n')
+finally: #예외 발생 여부와 관계없이 동작
+    print('프로그램 종료')
