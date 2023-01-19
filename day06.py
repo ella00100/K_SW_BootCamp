2d594f393f462a55ac2e38baa885c506143b2b31
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-19 10:42:11 +0900

    day06_decorator_@

--- day06.py
+++ day06.py
@@ -14,5 +14,15 @@ def document_it(func): #decorator 생성
     return new_function
 
 print(sub_int(7,3))
-info_sub_int = document_it(sub_int)
+info_sub_int = document_it(sub_int)        #수동할당
 info_sub_int(7,3)
+
+@document_it    #자동할당
+def sub_int(x,y):
+    return x - y
+@document_it
+def squrare(n):
+    return n*n
+
+print(sub_int(7,3))
+print(squrare(5))
