c1651c5e2b4ceff30b341364367a252af816eef7
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-20 15:16:36 +0900

    day07_duck_typing

--- day07.py
+++ day07.py
@@ -1,24 +1,53 @@
-#getter/setter 매서드
+class Shape:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
 
-class Duck():
-    def __init__(self,input_name):
-        self.hidden_name = input_name
+    def get_area(self):
+        print('도형의 면적을 출력합니다.')
 
-    def get_name(self):
-        print('inside the getter')
-        return self.hidden_name
+class Circle(Shape):
+    def __init__(self,x,y,radius):
+        super().__init__(x,y)
+        self.radius = radius
 
-    def set_name(self, input_name):
-        print('inside the setter')
-        self.hidden_name = input_name
+    def get_area(self):
+        import math
+        return math.pi * (self.radius**2)
 
-    name = property(get_name, set_name)
 
-don = Duck('Donald')
-print(don.get_name())
+class Rectangle(Shape):
+    def __init__(self, x, y, width, length):
+        super().__init__(x, y)
+        self.width = width
+        self.length = length
 
-don.name='Donna'
-print(don.name)
+    def get_area(self):
+        import math
+        return self.width * self.length
 
-don.set_name('InDuck')
-print(don.name)
+class Triangle(Shape):
+    def __init__(self, x, y, base, heigh):
+        super().__init__(x, y)
+        self.base = base
+        self.heigh = heigh
+
+    def get_area(self):
+        return (self.base * self.heigh)/2
+
+
+class Cylinder(Circle):
+    def __init__(self, x, y, radius,height):
+        super().__init__(x, y,radius)
+        self.height = height
+
+    def get_area(self):
+        return super().get_area() * self.height
+
+c1 = Circle(100,100, 10)
+c2 = Circle(50,50,2.0)
+r1 = Rectangle(100,50, 5,2)
+c3 = Cylinder(10,10,10,10)
+print(c3.get_area())
+
+print(f'사각형의 좌표는 x: {r1.x}, y: {r1.y}이고 넓이는 {r1.get_area()}입니다.')
