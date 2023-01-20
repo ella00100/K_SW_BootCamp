3bb8b4be4fc1d8327813d4a2f7c33dc3ca32f5b5
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-20 15:39:04 +0900

    day07_magic method

--- day07.py
+++ day07.py
@@ -6,16 +6,6 @@ class Shape:
     def get_area(self):
         print('도형의 면적을 출력합니다.')
 
-class Circle(Shape):
-    def __init__(self,x,y,radius):
-        super().__init__(x,y)
-        self.radius = radius
-
-    def get_area(self):
-        import math
-        return math.pi * (self.radius**2)
-
-
 class Rectangle(Shape):
     def __init__(self, x, y, width, length):
         super().__init__(x, y)
@@ -26,28 +16,16 @@ class Rectangle(Shape):
         import math
         return self.width * self.length
 
-class Triangle(Shape):
-    def __init__(self, x, y, base, heigh):
-        super().__init__(x, y)
-        self.base = base
-        self.heigh = heigh
+    def __repr__(self):  #magic method
+        return f'사각형의 좌표는 x: {self.x}, y: {self.y}이고 넓이는 {self.get_area()}입니다.'
 
-    def get_area(self):
-        return (self.base * self.heigh)/2
-
-
-class Cylinder(Circle):
-    def __init__(self, x, y, radius,height):
-        super().__init__(x, y,radius)
-        self.height = height
-
-    def get_area(self):
-        return super().get_area() * self.height
-
-c1 = Circle(100,100, 10)
-c2 = Circle(50,50,2.0)
+    def __add__(self, other):
+        # 넓이의 단순 합
+        # return self.get_area() + other.get_area()
+        # 각각의 사각형의 밑변과 높이를 더해 만든 사각형의 넓이
+        return Rectangle(0, 0, self.width + other.width, self.length+other.length)
 r1 = Rectangle(100,50, 5,2)
-c3 = Cylinder(10,10,10,10)
-print(c3.get_area())
+print(r1)
 
-print(f'사각형의 좌표는 x: {r1.x}, y: {r1.y}이고 넓이는 {r1.get_area()}입니다.')
+r2 = Rectangle(100,100,10,5)
+print(r2)
