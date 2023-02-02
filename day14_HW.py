d0e3713b461f26b88e7b70bc8b07fc9c78de4405
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-02 17:19:09 +0900

    day14_prob09

--- day14_HW.py
+++ day14_HW.py
@@ -1,40 +1,20 @@
-# prob 10.8
+#prob 10.9
 
-class Element:
-    def __init__(self,name, symbol, number):
-        self.__name = name
-        self.__symbol = symbol
-        self.__number = number
+class Bear():
+    def eats(selfs):
+        return "'berries' (Bear)"
 
-    def dump(self):
-        print(vars(self))
+class Rabbit:
+    def eats(self):
+        return "'clover' (Rabbit)"
 
-    @property
-    def get_name(self):
-        return self.__name
+class Octothorpe:
+    def eats(self):
+        return "'campers' (Ococthorpe)"
 
-    @property
-    def get_symbol(self):
-        return self.__symbol
-
-    @property
-    def get_number(self):
-        return  self.__number
-
-    @get_name.setter
-    def name_setter(self,name):
-        self.__name = name
-
-    @get_symbol.setter
-    def symbol_setter(self,symbol):
-        self.__symbol = symbol
-
-    @get_number.setter
-    def number_setter(self,number):
-        self.__number = number
-
-hydrogen = Element('Hydrogen','H',1)
-print(hydrogen.get_name)
-
-hydrogen.name_setter = 'Hydrrrgen'
-print(hydrogen.get_name)
+bear = Bear()
+rabbit = Rabbit()
+ocot = Octothorpe()
+print(bear.eats())
+print(rabbit.eats())
+print(ocot.eats())
