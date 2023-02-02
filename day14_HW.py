f8e5f34da0117a084c23a9e26d1e1da5b4be47c2
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-02 17:18:52 +0900

    day14_prob08

--- day14_HW.py
+++ day14_HW.py
@@ -1,27 +1,40 @@
-#prob 10.7
+# prob 10.8
 
-class Element():
-    def __init__(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
+class Element:
+    def __init__(self,name, symbol, number):
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
 
     def dump(self):
         print(vars(self))
 
+    @property
+    def get_name(self):
+        return self.__name
+
+    @property
+    def get_symbol(self):
+        return self.__symbol
+
+    @property
+    def get_number(self):
+        return  self.__number
+
+    @get_name.setter
+    def name_setter(self,name):
+        self.__name = name
+
+    @get_symbol.setter
+    def symbol_setter(self,symbol):
+        self.__symbol = symbol
+
+    @get_number.setter
+    def number_setter(self,number):
+        self.__number = number
+
 hydrogen = Element('Hydrogen','H',1)
-hydrogen.dump()
+print(hydrogen.get_name)
 
-class Element():
-    def __init__(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
-
-    def __str__(self):
-        return f'{vars(self)}'
-
-hydrogen = Element('Hydrogen','H',1)
-
-print(hydrogen)
-print(dir(Element))
+hydrogen.name_setter = 'Hydrrrgen'
+print(hydrogen.get_name)
