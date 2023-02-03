3337e26671290c79895b4433c4449cb55145a9c3
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-03 11:15:44 +0900

    day15_linear_list_insert_data

--- day15.py
+++ day15.py
@@ -1,14 +1,22 @@
-pokemons = []  # 빈 배열
+pokemons = ["피카츄", "라이츄", "파이리", "꼬부기", "이상해"]
 
-def add_data(pokemon):
-    pokemons.append(None)      #공간 확보
-    #pokemons[len(pokemons) - 1] = pokemon  #마지막 번지에 pokemon추가
-    pokemons[-1] = pokemon     #위와 같은 코드
 
-add_data('피카츄')
-add_data('라이츄')
-add_data('파이리')
-add_data('꼬부기')
-add_data('이상해')
+def insert_data(idx, pokomon):
+    if idx < 0 or idx > len(pokemons):
+        print("데이터를 삽입할 범위를 벗어났습니다.")
+        return
 
+    pokemons.append(None)  # 빈칸 추가
+    kLen = len(pokemons)  # 배열의 현재 크기
+
+    for i in range(kLen - 1, idx, -1):
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
+
+    pokemons[idx] = pokomon  # 지정한 위치에 친구 추가
+
+print(pokemons)
+insert_data(2, "라도란")
+print(pokemons)
+insert_data(6, "버터플")
 print(pokemons)
