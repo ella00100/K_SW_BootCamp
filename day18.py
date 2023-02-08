51c5fcebb06df281e7927fcc5cf59b06c8acc7ee
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-08 15:25:52 +0900

    day18 graph Traversal 04

--- day18.py
+++ day18.py
@@ -1,58 +1,103 @@
-from collections import deque
-
+## 클래스 및 함수 선언 부분 ##
 class Graph() :
     def __init__ (self, size) :
         self.SIZE = size
         self.graph = [[0 for _ in range(size)] for _ in range(size)]
 
-G1 = None
-queue = deque([])
-visited_array = []		# 방문한 정점
-
-#정점생성
-G1 = Graph(9)
-
-#엣지 생성, 연결
-G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][4] = 1
-G1.graph[1][0] = 1; G1.graph[1][2] = 1; G1.graph[1][3] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1; G1.graph[2][4] = 1; G1.graph[2][5] = 1;
-G1.graph[3][1] = 1; G1.graph[3][2] = 1
-G1.graph[4][0] = 1; G1.graph[4][2] = 1; G1.graph[4][6] = 1; G1.graph[4][7] = 1
-G1.graph[5][2] = 1
-G1.graph[6][5] = 1; G1.graph[6][8] = 1
-G1.graph[7][4] = 1; G1.graph[7][8] = 1
-G1.graph[8][6] = 1; G1.graph[8][7] = 1
-
-
-#그래프 출력
-for row in range(9) :
-    for col in range(9) :
-        print(G1.graph[row][col], end = ' ')
+def printGraph(g) :
+    print('   ', end = ' ')
+    for first_row in range(g.SIZE) :   # 첫 행에 정점 출력
+        print(name_array[first_row], end =' ')
+    print()
+    for row in range(g.SIZE) : # 한 행씩 돌면서
+        print(name_array[row], end =' ')
+        for col in range(g.SIZE) : # 각 열에 엣지 출력
+            print("%3d" % g.graph[row][col], end = ' ')
+        print()
     print()
 
-#깊이 우선 탐색
-current = 0		# 시작 정점 A
-queue.append(current)
-visited_array.append(current)
+def findVertex(g, findVtx) :
+    stack = []
+    visited_array = []	# 방문한 노드
 
-while (len(queue) != 0) :
-    next = None
-    for vertex in range(9) :                #한 행(정점)씩 돌면서
-        if G1.graph[current][vertex] == 1 : #연결된 도착점 찾기
-            if vertex in visited_array :  	   # 방문한 적이 있는 정점이면 탈락
-                pass
-            else : 			   # 방문한 적이 없으면 다음 정점으로 지정
-                next = vertex
-                break
+    current = 0	# 시작 정점
+    stack.append(current)
+    visited_array.append(current)
 
-    if next is not None :			  	   # 다음에 방문할 정점이 있는 경우
-        current = next                 # 다음으로 이동하여 방문내역 추가
-        queue.append(current)          # enQueue
-        visited_array.append(current)
-    else :            	  	  	  	  # 다음에 방문할 정점이 없는 경우
-#        current = queue.pop() ## O(n)OVERHEAD (Queue 사용 시 문제점) ##
-         current = queue.popleft() #O(1) deQueue
+    while (len(stack) != 0) :
+        next = None
+        for vertex in range(gSize) :
+            if g.graph[current][vertex] != 0 :
+                if vertex in visited_array :	# 방문한 적이 있는 정점이면 탈락
+                    pass
+                else :			# 방문한 적이 없으면 다음 정점으로 지정
+                    next = vertex
+                    break
 
-print('방문 순서 --> ', end='')
-for i in visited_array :
-    print(chr(ord('A')+i), end='   ')
+        if next != None :				# 다음에 방문할 정점이 있는 경우
+            current = next
+            stack.append(current)
+            visited_array.append(current)
+        else :					# 다음에 방문할 정점이 없는 경우
+            current = stack.pop()
+
+    if findVtx in visited_array :
+        return True
+    else :
+        return False
+
+
+## 전역 변수 선언 부분 ##
+G1 = None
+name_array = ['춘천', '서울', '속초', '대전', '광주', '부산']
+춘천, 서울, 속초, 대전, 광주, 부산 = 0, 1, 2, 3, 4, 5
+
+
+## 메인 코드 부분 ##
+gSize = 6
+G1 = Graph(gSize)
+G1.graph[춘천][서울] = 10; G1.graph[춘천][속초] = 15
+G1.graph[서울][춘천] = 10; G1.graph[서울][속초] = 40; G1.graph[서울][대전] = 11; G1.graph[서울][광주] = 50
+G1.graph[속초][춘천] = 15; G1.graph[속초][서울] = 40; G1.graph[속초][대전] = 12
+G1.graph[대전][서울] = 11; G1.graph[대전][속초] = 12; G1.graph[대전][광주] = 20; G1.graph[대전][부산] = 30
+G1.graph[광주][서울] = 50; G1.graph[광주][대전] = 20; G1.graph[광주][부산] = 25
+G1.graph[부산][대전] = 30; G1.graph[부산][광주] = 25
+
+print('## 자전거 도로 건설을 위한 전체 연결도 ##')
+printGraph(G1)
+
+# 가중치 간선 목록
+edgeAry = []
+for i in range(gSize) :
+	for k in range(gSize) :
+		if G1.graph[i][k] != 0 :
+			edgeAry.append([G1.graph[i][k], i, k])
+
+from operator import itemgetter
+edgeAry = sorted(edgeAry, key = itemgetter(0), reverse = True)
+
+newAry = []
+for i in range(0,len(edgeAry), 2) :
+	newAry.append(edgeAry[i])
+
+index = 0
+while (len(newAry) > gSize-1) :	# 간선의 개수가 '정점 개수-1'일 때까지 반복
+	start = newAry[index][1]
+	end = newAry[index][2]
+	saveCost = newAry[index][0]
+
+	G1.graph[start][end] = 0
+	G1.graph[end][start] = 0
+
+	startYN = findVertex(G1, start)
+	endYN = findVertex(G1, end)
+
+	if startYN and endYN :
+		del (newAry[index])
+	else :
+		G1.graph[start][end] = saveCost
+		G1.graph[end][start] = saveCost
+		index += 1
+
+print('## 최소 비용의 자전거 도로 연결도 ##')
+printGraph(G1)
