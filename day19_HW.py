18b86d4e5a3ebf40fd093e2285a7bada2f34882e
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-09 18:43:47 +0900

    day19 prob 11.2

--- day19_HW.py
+++ day19_HW.py
@@ -1,21 +1,25 @@
+def two_to_one(ary):
+    one_array = []
+    for i in range(len(ary)):
+        for j in range(len(ary)):
+            one_array.append(ary[i][j])
+    return one_array
+
+
 def insertionSort(ary):
     for end in range(1,len(ary)):
         for cur in range(end, 0, -1):
-            if ary[cur-1][1] > ary[cur][1]:
+            if ary[cur-1] > ary[cur]:
                 ary[cur-1], ary[cur] = ary[cur], ary[cur-1]
     return ary
 
-def twobytwo(ary):
-    groub = list()
-    i = 0
-    while i < len(ary):
-        groub = ary[i][0],ary[i+1][0]
-        i += 2
-        print(f'{groub[0]} : {groub[1]}')
+data_array = [[55,33,250,44],
+              [88,1,67,23],
+              [199,222,38,47],
+              [155,245,20,99]]
 
-data_array = [['선미',88], ['초아',99],['화사',71],['영탁',78],['영웅',67],['민호',92]]
-print(f'정렬 전 ---> {data_array}')
-sorted_array = insertionSort(data_array)
-print(f'정렬 후 ---> {sorted_array}' )
-print('## 성적별 조 편성표 ##')
-twobytwo(sorted_array)
+one_array = two_to_one(data_array)
+print(f'1차원 변경 후, 정렬 전 ---> {one_array}')
+sorted_array = insertionSort(one_array)
+print(f'1차원 변경 후, 정렬 후 ---> {sorted_array}' )
+print(f'중앙값 : {sorted_array[len(sorted_array)//2]}')
