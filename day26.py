ee0451cb76c0ea5706dc088449afdcedba5b38fd
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-20 20:22:41 +0900

    day26HW prob02

--- day26.py
+++ day26.py
@@ -1,48 +1,51 @@
 import random
 
-def seq_search(ary, find_data):
+def seq_search(ary, find_data) :
+    global count
     pos = -1
-    size = len(ary)
-    for i in range(size):
-        print(ary[i], end = ' ')
-        if ary[i] == find_data:
+    for i in range(len(ary)) :
+        count += 1
+        if ary[i] == find_data :
             pos = i
             break
-    print()
     return pos
 
 def binary_search(ary, find_data) :
+    global count
     start = 0
     end = len(ary) - 1
+
     while (start <= end) :
+        count += 1
         mid = (start + end) // 2
+
         if find_data == ary[mid] :
             return mid
         elif find_data > ary[mid] :
             start = mid + 1
         else :
             end = mid - 1
+
     return -1
 
-data_ary = ['바나나맛우유', '레쓰비캔커피', '츄파춥스', '도시락', '삼다수', '코카콜라', '삼각김밥']
-sell_ary = [random.choice(data_ary) for _ in range(20)]
+data_ary= []
+find_data = 121922
+count = 0
 
-## 메인 코드 부분 ##
-print(f'#오늘 판매된 전체 물건(중복O, 정렬X) --> {sell_ary}')
-sell_ary.sort()
-print(f'#오늘 판매된 전체 물건(중복O, 정렬O) --> {sell_ary}')
-sell_product = list(set(sell_ary))
-print('#오늘 판매된 물품 종류(중복x) -->', sell_product)
-count_list = []
-for product in sell_product :
-    count = 0
-    pos = 0
-    while (pos != -1) :
-        pos = binary_search(sell_ary, product)
-        if pos != -1 :
-            count += 1
-            del(sell_ary[pos])
-    count_list.append( (product, count))
+data_ary = [random.randint(0, 999999) for _ in range(1000000)]
+data_ary.insert(random.randint(0, 1000000), find_data)
 
-print()
-print("결산 결과 ==>", count_list)
+count = 0
+print(f'#비정렬 배열(100만개) --> {data_ary[0:5]} ~~~~ {data_ary[-5:len(data_ary)]}')
+pos_seq = seq_search(data_ary,find_data)
+count_seq = count
+
+data_ary.sort()
+count = 0
+print(f'#정렬 배열(100만개) --> {data_ary[0:5]} ~~~~ {data_ary[-5:len(data_ary)]}')
+pos_binary = binary_search(data_ary, find_data)
+count_binary = count
+
+
+print(f'순차 검색(비정렬 데이터) --> {count_seq}회')
+print(f'이진 검색(정렬 데이터) --> {count_binary}회')
