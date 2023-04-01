b98996ff1333cf9fdd0a773b8a308769b3111871
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-04-02 01:18:11 +0900

    yose

--- coding_test/week2.py
+++ coding_test/week2.py
@@ -1,27 +1,12 @@
-import sys
-sys.setrecursionlimit(10**6)
+from sys import stdin
 
-n,m = map(int, sys.stdin.readline().rstrip().split())
+n, k = map(int, stdin.readline().split())
+num = list(range(1, n+1))
+i = 0
+result = []
 
-node = [[] for _ in range(n+1)]
-sangsa_num = list(map(int, sys.stdin.readline().rstrip().split()))
-cing = [0 for _ in range(n + 1)]
+while num:
+    i = (i + k - 1) % len(num)
+    result.append(num.pop(i))
 
-for i in range(1, n+1):
-    if sangsa_num[i - 1] != -1:
-        node[sangsa_num[i - 1]].append(i)
-
-for j in range(m):
-    num, w = map(int, sys.stdin.readline().rstrip().split())
-    cing[num] += w
-
-stack = [1]
-while stack:
-    curr = stack.pop()
-    for k in node[curr]:
-        cing[k] += cing[curr]
-        stack.append(k)
-
-cing = cing[1:]
-for i in cing:
-    print(i, end=" ")
+print('<' + ', '.join(map(str, result)) + '>')
