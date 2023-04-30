230534c11efa16e4ffe46a77ee9bb8ef01796d39
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-04-30 18:27:39 +0900

    island

--- coding_test/week3.py
+++ coding_test/week3.py
@@ -1,29 +1,43 @@
-from sys import stdin
-from sys import setrecursionlimit
-setrecursionlimit(10**6) #재귀 깊이 제한 풀기
+#섬의 개수
+import sys
+sys.setrecursionlimit(10000)
 
-def dfs(node):
-    visited[node] = 1
-    for child in graph[node]:
-        if not visited[child]:
-            parent[child] = node
-            dfs(child)
+def DFS(i, j, w, h, array, visited):
+    if visited[i][j]:
+        return 0
+    if array[i][j]:
+        return 0
 
-n = int(input())
-graph = [[] for _ in range(n+1)]
-visited = [0] * (n+1)
-parent = [0] * (n+1)
+    visited[i][j] = True
+    for dx, dy in [[-1,-1], [-1,0], [-1,1],
+                   [0,-1], [0,1],
+                   [1,-1], [1,0], [1,1]]:
+        nx = i + dx
+        ny = j + dy
 
-for i in range(n-1):
-        a, b = map(int, stdin.readline().rstrip().split())
-        graph[a].append(b)
-        graph[b].append(a)
-        i += 1
-for i in range(n+1):
-    graph[i].sort()
+        if 0<= nx < h and 0 <= ny < w:
+            if array[nx][ny] == 1:
+                DFS(nx, ny, w, h, array, visited)
 
-dfs(1)
+    return 1
 
-parent.pop(0)
-parent.pop(0)
-print(*parent, sep='\n')
+def solution(w, h, array):
+    answer = 0
+    visited =[[False]*w for _ in range(h)]
+    for i in range(h):
+        for j in range(w):
+            if not visited[i][j]:
+                answer = answer + DFS(i, j, w, h, array, visited)
+
+    return answer
+
+while True:
+    w, h = map(int, input().split())
+    if w == h == 0:
+        break
+    array = []
+    for _ in range(h):
+        array.append(list(map(int, input().split())))
+
+    answer = solution(w, h, array)
+    print(answer)
