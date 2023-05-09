11cec02429c53cf7873303cc1d1e701c59e8d717
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-05-09 22:50:32 +0900

    pob 1

--- coding_test/week3.py
+++ coding_test/week3.py
@@ -1,60 +1,26 @@
-import sys
-from collections import deque
-from copy import deepcopy
+n = int(input())
 
-input = sys.stdin.readline
-sys.setrecursionlimit(10000)
+pre = []
+for i in range(n):
+    p = input().split()
+    pre.append((p[0], p[-1]))
 
-w, h = map(int, input().split())
-array = []
+for i in range(n):
+    for j in range(n):
+        if pre[i][1] == pre[j][0]:
+            pre.append((pre[i][0], pre[j][1]))
 
-for _ in range(w):
-    array.append(list(map(int, input().split())))
+m = int(input())
+que = []
 
-def build_wall(count):
-    global ans
+for i in range(m):
+    q = input().split()
+    que.append((q[0], q[-1]))
 
-    if count == 3:
-        bfs(deepcopy(array))
-        return
-
-    for i in range(w):
-        for j in range(h):
-            if array[i][j] == 0:
-                array[i][j] = 1
-                build_wall(count + 1)
-                array[i][j] = 0
-
-def bfs(array):
-    global ans
-
-    virus = deepcopy(array)
-
-    queue = deque()
-
-    for i in range(w):
-        for j in range(h):
-            if virus[i][j] == 2:
-                queue.append((i, j))
-
-    while queue:
-        x, y = queue.popleft()
-        for dx, dy in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
-            nx = x + dx
-            ny = y + dy
-            if 0 <= nx < w and 0 <= ny < h:
-                if virus[nx][ny] == 0:
-                    virus[nx][ny] = 2
-                    queue.append((nx, ny))
-
-    count = 0
-    for i in range(w):
-        for j in range(h):
-            if virus[i][j] == 0:
-                count += 1
-
-    ans = max(ans, count)
-
-ans = 0
-build_wall(0)
-print(ans)
+for i in range(m):
+    result = False
+    for j in pre:
+        if que[i] == j:
+            result = True
+            break
+    print('T' if result else 'F')
