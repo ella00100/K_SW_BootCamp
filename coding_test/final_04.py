6a17065ba9a650c971cf7e8b95da49a764a389fc
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-05-31 00:26:57 +0900

    final_04

---
+++ coding_test/final_04.py
@@ -0,0 +1,7 @@
+from sys import stdin
+
+p, w = map(int, stdin.readline().split())
+c, v = map(int, stdin.readline().split())
+
+for i in range(w):
+    ws, we, ww = map(int, stdin.readline().split())
